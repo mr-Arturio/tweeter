11f016e00079176ff9580e25582ce534bd0169c1
ArturT <mr.arturio@gmail.com>
2023-03-21 18:10:29 -0400

    create a JavaScript function that will generate the DOM structure for a tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,22 +7,77 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function (){
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    for (const tweetData of tweets) {
+           // calls createTweetElement for each tweet
+    const $tweet = createTweetElement(tweetData);
+    // takes return value and appends it to the tweets container
+      $('#tweets-container').append($tweet);
+    }
+  };
+
+
+  const createTweetElement = function (tweet) {
+    const user = tweet.user;
+    const content = tweet.content;
+    const created_at = tweet.created_at;
+    let $tweet = $(`
+    <article class="tweet">
+
+      <header>
+        <div class="avatar">
+          <img src="${user.avatars}" alt="User Avatar">
+          <h3>${user.name}</h3>
+        </div>
+        <span class="handle">${user.handle}</span>
+      </header>
+  
+      <div class="content">
+        <p>${content.text}</p>
+      </div>
+  
+      <footer>
+        <div class="date">${created_at}</div>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-thumbs-up"></i>
+        </div>
+        </footer>
+
+      </article>
+    `)
+  return $tweet;
   }
 
-  const $tweet = $(`<article class="tweet">Hello world</article>`);
-
-  $('#tweets-container').append($tweet);
-
+  renderTweets(data);
 });
 
 
